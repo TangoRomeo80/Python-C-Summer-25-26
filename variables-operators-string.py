5dd58b3e5b8e898dd2fd6d8702864464e08cd49d
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-06-15 09:42:49 +0600

    Update variables-operators-string.py

--- variables-operators-string.py
+++ variables-operators-string.py
@@ -1,1 +1,20 @@
-print("Hello")
+# a = 10
+# b = 3
+
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Float Division:", a / b)
+# print("Integer Division:", a // b)
+# print("Modulus:", a % b)
+# print("Exponentiation:", a ** b)
+
+# a = 10
+# b = 20
+# c = 15
+# print((a + b + c) / 3)
+
+# a = "1"
+# b = "1"
+# print(a + b)
+
