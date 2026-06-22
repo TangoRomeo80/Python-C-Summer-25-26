29e7c8acf9f863127c6a160cd9b8c1db269625d7
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-06-22 10:04:56 +0600

    Added control flow codes

--- control-flow.py
+++ control-flow.py
@@ -18,24 +18,66 @@
 # else:
 #     print("Enjoy the sun")
 
-day = 3
-match day:
-    # case 1 | 2 | 3 | 4 | 5:
-    #     print("Weekday")
-    case 1:
-        print("Monday")
-    case 2:
-        print("Tuesday")
-    case 3:
-        print("Wednesday")
-    case 4:
-        print("Thursday")
-    case 5:
-        print("Friday")
-    case 6:
-        print("Saturday")
-    case 7:
-        print("Sunday")
-    case _:
-        print("Invalid day")
+# day = 3
+# match day:
+#     # case 1 | 2 | 3 | 4 | 5:
+#     #     print("Weekday")
+#     case 1:
+#         print("Monday")
+#     case 2:
+#         print("Tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday")
+#     case _:
+#         print("Invalid day")
 
+# x = 10
+# while True:
+#     print(x)
+#     x -= 1
+#     if x == 5:
+#         x -= 1
+#         continue
+#     if x == 0:
+#         break
+
+# fruits = ["apple", "banana", "mango"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# print(list(range(5)))
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1, 5):
+#     print(i)
+
+# for i in range(1, 6, 2):
+#     print(i)
+
+# for i in range(10, 4, -2):
+#     print(i)
+
+# n = int(input("Enter a number: "))
+# if n % 3 == 0:
+#     print("Argentina")
+# elif n % 3 == 0 and n % 4 == 0 and n % 5 == 0:
+#     print("Not Possible")
+# elif n % 3 == 0 and n % 4 == 0:
+#     print("in future")
+# elif n % 4 == 0:
+#     print("Germany")
+# elif n % 5 == 0:
+#     print("Brazil")
+# else:
+#     print(n)
---
+++ functions.py
@@ -0,0 +1,1 @@
+# This file contains functions related codes
