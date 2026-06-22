ce26cd3891806bb0e2bef3880a3547b853785c36
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-06-22 10:19:51 +0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,13 @@
 # This file contains functions related codes
+
+def function_name(parameters):
+    # function body
+    pass
+
+# def add(a, b):
+#     return a + b
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+print(add(1, 4))
