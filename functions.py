1646a8cb248cc6536efe93d38fe863bfd47edf6b
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2026-06-24 10:05:14 +0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,7 +7,45 @@ def function_name(parameters):
 # def add(a, b):
 #     return a + b
 
-def add(a: int, b: int) -> int:
-    return a + b
+# def add(a: int, b: int) -> int:
+#     return a + b
 
-print(add(1, 4))
+# print(add(1, 4))
+
+# Function with default parameters
+# def greet(name : str = "World") -> str:
+#     return f"Hello, {name}!"
+
+# print(greet())  # Output: Hello, World!
+# print(greet("Alice"))  # Output: Hello, Alice!
+
+# # Keyword arguments
+# def print_info(age: int,name: str = "Bob") -> None:
+#     print(f"Name: {name}, Age: {age}")
+
+# print_info(age=25)
+
+# x = 10
+# while True:
+#     # x = 10
+#     print(x)
+#     if x == 5:
+#         break
+#     x -= 1 # x = x -1
+# print(x)
+
+# def show_text() -> None:
+#     text = "Hello, World!"
+#     print(text)
+
+# show_text()
+# print(text) # This variable is not defined in this scope
+
+# Recursive function to print a string in reverse
+# def reverse_string(s: str) -> str:
+#     if s == "":
+#         return s
+#     else:
+#         return reverse_string(s[1:]) + s[0]
+    
+# print(reverse_string("Hel"))  # Output: leH
